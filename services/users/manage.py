59274ce21ec31ac288c790c26c629f9228e8b304
tyler reitz <tdreitz@gmail.com>
2019-01-30 10:50:54 -0800

    Part 1, Chapter 8: Test Setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,3 +1,5 @@
+import unittest
+
 from flask.cli import FlaskGroup
 
 from project import app, db
@@ -11,6 +13,16 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """Run test withou code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
