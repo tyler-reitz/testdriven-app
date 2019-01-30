e0bd711df84574b159941cb234663f1920af5c50
tyler reitz <tdreitz@gmail.com>
2019-01-30 09:19:57 -0800

    Part 1, Chapter 6: Docker Config

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
-@app.route('/ping', methods=["GET"])
+@app.route('/users/ping', methods=["GET"])
 def ping_pong():
     return jsonify({
         'status': 'success',
