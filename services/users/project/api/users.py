a9ffcaef45317eb8df207b8dd6a72bebe1183708
tyler reitz <tdreitz@gmail.com>
2019-01-31 08:56:35 -0800

    Part 1, Chapter 12: Jinja Templates

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from sqlalchemy import exc
 
 from project.api.models import User
 from project import db
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 
 @users_blueprint.route('/users/ping', methods=['GET'])
 def ping_pong():
@@ -12,6 +12,18 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+
+@users_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
 
 @users_blueprint.route('/users', methods=['POST'])
 def add_user():
@@ -75,3 +87,4 @@ def get_all_users():
         }
     }
     return jsonify(response_object), 200
+
