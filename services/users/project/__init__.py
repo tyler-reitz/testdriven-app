ca4185e6c7fef53f8d9e931182bb203b58abe631
tyler reitz <tdreitz@gmail.com>
2019-01-31 12:27:13 -0800

    Part 2, Chapter 4: Flask Debug Toolbar

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -17,6 +19,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -21,6 +21,7 @@ class TestDevelopmentConfig(TestCase):
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_URL')
         )
+        self.assertTrue(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestTestingConfig(TestCase):
@@ -31,11 +32,12 @@ class TestTestingConfig(TestCase):
     def test_app_is_testing(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertTrue(app.config['TESTING'])
-        self.assertFalse(app.config('PRESERVE_CONTEXT_ON_EXCEPTION'))
+        self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URL'] ==
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_TEST_URL')
         )
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestProductionConfig(TestCase):
@@ -46,6 +48,7 @@ class TestProductionConfig(TestCase):
     def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 if __name__ == '__main__':
