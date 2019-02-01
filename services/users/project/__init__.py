4ef910bbf054fd712b0c568215559ef21a41db9b
tyler reitz <tdreitz@gmail.com>
2019-01-31 18:17:47 -0800

    Part 2, Chapter 5: React Setup

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
+cors = CORS()
 
 
 def create_app(script_info=None):
@@ -20,6 +22,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)
+    cors.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
