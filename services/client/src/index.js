d86a4d6036d2210dc983c4cf14a6f6e1351a2d1e
tyler reitz <tdreitz@gmail.com>
2019-01-31 21:35:31 -0800

    Part 2, Chapter 8: React and Docker

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
-  constructor() {
-    super()
+  constructor() { super()
     this.state = {
-      users: []
+      users: [],
+      username: '',
+      email: '',
     }
   }
 
@@ -22,6 +24,28 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  addUser = (event) => {
+    event.preventDefault();
+
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    }
+
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({ username: '', email: '' })
+      })
+      .catch((err) => { console.log(err); })
+  }
+
+  handleChange = (event) => {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
   render() {
     return (
       <section className="section">
@@ -30,6 +54,13 @@ class App extends Component {
             <div className="column is-one-third">
               <br/>
               <h1 className="title is-1 is-1">All Users</h1>
+              <hr/><br/>
+              <AddUser
+                addUser={this.addUser}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
               <hr/><br/>
               <UsersList users={this.state.users} />
             </div>
