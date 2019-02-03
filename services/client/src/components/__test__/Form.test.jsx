64561e8977018a923c00b13ee0c527e13b9a9d41
tyler reitz <tdreitz@gmail.com>
2019-02-03 06:50:27 -0800

    Part 3, Chapter 9: Mocking User Interaction

--- services/client/src/components/__test__/Form.test.jsx
+++ services/client/src/components/__test__/Form.test.jsx
@@ -4,44 +4,73 @@ import renderer from 'react-test-renderer'
 
 import Form from '../Form'
 
-const formData = {
-  username: '',
-  email: '',
-  password: ''
-}
+const testData = [
+  {
+    formType: 'Register',
+    title: 'Register',
+    formData: {
+      username: '',
+      email: '',
+      password: ''
+    },
+    handleUserFormSubmit: jest.fn(),
+    handleFormChange: jest.fn(),
+    isAuthenticated: false
+  },
+  {
+    formType: 'Login',
+    title: 'Log In',
+    formData: {
+      email: '',
+      password: ''
+    },
+    handleUserFormSubmit: jest.fn(),
+    handleFormChange: jest.fn(),
+    isAuthenticated: false
+  },
+]
 
-it('Register Form renders properly', () => {
-  const component = <Form formType={'Register'} formData={formData} />
-  const wrapper = shallow(component)
-  const h1 = wrapper.find('h1')
-  expect(h1.length).toBe(1)
-  expect(h1.get(0).props.children).toBe('Register');
-  const formGroup = wrapper.find('.field')
-  expect(formGroup.length).toBe(3)
-  expect(formGroup.get(0).props.children.props.name).toBe('username')
-  expect(formGroup.get(0).props.children.props.value).toBe('')
-})
+describe('When not authenticated', () => {
+  testData.forEach((el) => {
+    const component = <Form {...el} />
+    it(`${el.formType} Form renders properly`, () => {
+      const wrapper = shallow(component)
+      const h1 = wrapper.find('h1')
+      expect(h1.length).toBe(1)
+      expect(h1.get(0).props.children).toBe(el.title);
+      const formGroup = wrapper.find('.field')
+      expect(formGroup.length).toBe(Object.keys(el.formData).length)
+      expect(formGroup.get(0).props.children.props.name).toBe(Object.keys(el.formData)[0])
+      expect(formGroup.get(0).props.children.props.value).toBe('')
+    })
+    it(`${el.formType} Form submits form properly`, () => {
+      const wrapper = shallow(component)
+      const input = wrapper.find('input[type="email"]')
+      expect(el.handleUserFormSubmit).toHaveBeenCalledTimes(0)
+      expect(el.handleFormChange).toHaveBeenCalledTimes(0)
+      input.simulate('change')
+      expect(el.handleFormChange).toHaveBeenCalledTimes(1)
+      wrapper.find('form').simulate('submit', el.formData)
+      expect(el.handleUserFormSubmit).toHaveBeenCalledWith(el.formData)
+      expect(el.handleUserFormSubmit).toHaveBeenCalledTimes(1)
+    })
+    it(`${el.formType} Form renders a snapshot properly`, () => {
+      const tree = renderer.create(component).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+  })
+}) 
 
-it('Login Form renders properly', () => {
-  const component = <Form formType={'Login'} formData={formData} />
-  const wrapper = shallow(component)
-  const h1 = wrapper.find('h1')
-  expect(h1.length).toBe(1)
-  expect(h1.get(0).props.children).toBe('Log In');
-  const formGroup = wrapper.find('.field')
-  expect(formGroup.length).toBe(2)
-  expect(formGroup.get(0).props.children.props.name).toBe('email')
-  expect(formGroup.get(0).props.children.props.value).toBe('')
-})
-
-test('Register Form renders a snapshot properly', () => {
-  const component = <Form formType={'Register'} formData={formData} />
-  const tree = renderer.create(component).toJSON()
-  expect(tree).toMatchSnapshot()
-})
-
-test('Login Form renders a snapshot properly', () => {
-  const component = <Form formType={'Login'} formData={formData} />
-  const tree = renderer.create(component).toJSON()
-  expect(tree).toMatchSnapshot()
+describe('When authenticated', () => {
+  testData.forEach((el) => {
+    const component = <Form
+      formType={el.formType}
+      formData={el.formData}
+      isAuthenticated={true}
+    />
+    it(`${el.formType} redirects properly`, () => {
+      const wrapper = shallow(component)
+      expect(wrapper.find('Redirect')).toHaveLength(1)
+    })
+  })
 })
