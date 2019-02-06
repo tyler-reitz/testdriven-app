b78e127624cffd7c106f65a4fb73a095058da1c1
tyler reitz <tdreitz@gmail.com>
2019-02-06 14:56:21 -0800

    Part 4, Chapter 7: Update Test Script

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+type=$1
 fails=""
 
 inspect() {
@@ -9,22 +10,64 @@ inspect() {
 }
 
 
-# run unit and integration tests
-docker-compose -f docker-compose-dev.yml up -d --build
-docker-compose -f docker-compose-dev.yml exec users python manage.py test
-inspect $? users
-docker-compose -f docker-compose-dev.yml exec users flake8 project
-inspect $? users-lint
-docker-compose -f docker-compose-dev.yml exec client npm test -- --coverage
-inspect $? client
-docker-compose -f docker-compose-dev.yml down
+# run server-side tests
+server() {
+  docker-compose -f docker-compose-dev.yml up -d --build
+  docker-compose -f docker-compose-dev.yml exec users python manage.py test
+  inspect $? users
+  docker-compose -f docker-compose-dev.yml exec users flake8 project
+  inspect $? users-lint
+  docker-compose -f docker-compose-dev.yml down
+}
 
-docker-compose -f docker-compose-prod.yml up -d --build
-docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-npx cypress run --config baseUrl=http://localhost
-inspect $? e2e
-docker-compose -f docker-compose-prod.yml down
+# run client-side tests
+client() {
+  docker-compose -f docker-compose-dev.yml up -d --build
+  docker-compose -f docker-compose-dev.yml exec client npm test -- --coverage
+  inspect $? client
+  docker-compose -f docker-compose-dev.yml down
+}
 
+# run e2e tests
+e2e() {
+  docker-compose -f docker-compose-prod.yml up -d --build
+  docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+  npx cypress run --config baseUrl=http://localhost
+  inspect $? e2e
+  docker-compose -f docker-compose-prod.yml down
+}
+
+# run all tests
+all() {
+  docker-compose -f docker-compose-dev.yml up -d --build
+  docker-compose -f docker-compose-dev.yml exec users python manage.py test
+  inspect $? users
+  docker-compose -f docker-compose-dev.yml exec users flake8 project
+  inspect $? users-lint
+  docker-compose -f docker-compose-dev.yml exec client npm test -- --coverage
+  inspect $? client
+  e2e
+}
+
+
+# run appropriate tests
+if [[ "${type}" == "server" ]]; then
+  echo "\n"
+  echo "Running server-side tests!\n"
+  server
+elif  [[ "${type}" == "client" ]]; then
+  echo "\n"
+  echo "running client-side tests!\n"
+  client
+elif  [[ "${type}" == "e2e" ]]; then
+  echo "\n"
+  echo "running e2e tests!\n"
+  e2e
+else
+  echo "\n"
+  echo "Running all tests!\n"
+  all
+fi
 
 # return proper code
 if [ -n "${fails}" ]; then
